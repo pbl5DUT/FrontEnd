665d246edaa50b20ef2bffd9e96d19d46c57a054
Anhbinh272 <Hoangleanhbinhtin92017@gmail.com>
2025-05-11 13:01:23 +0700

    employee

---
+++ src/pages/employee/create.tsx
@@ -0,0 +1,15 @@
+// src/pages/employee/create.tsx
+'use client';
+
+import CreateEmployee from '@/modules/employee/components/EmployeeCreate/CreateEmployee';
+
+const CreateEmployeePage = () => {
+  return (
+    <div>
+    
+      <CreateEmployee />
+    </div>
+  );
+};
+
+export default CreateEmployeePage;
