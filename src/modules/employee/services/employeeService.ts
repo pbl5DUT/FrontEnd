a13753e0116e0e224e1f229e9146c093652a4a59
Anhbinh272 <Hoangleanhbinhtin92017@gmail.com>
2025-05-15 15:34:00 +0700

    employee api

--- src/modules/employee/services/employeeService.ts
+++ src/modules/employee/services/employeeService.ts
@@ -26,32 +26,33 @@ export interface Employee {
   created_at: string;
   enterprise: Enterprise;
 }
+
 export type CreateEmployeeDto = Omit<Employee, 'user_id' | 'created_at' | 'enterprise'> & {
   enterprise_id: string;
 };
 
 export const employeeService = {
   getEmployees: async (): Promise<Employee[]> => {
-    const response = await axiosInstance.get('http://127.0.0.1:8000/api/users/');
+    const response = await axiosInstance.get('/users/');
     return response.data;
   },
 
   getEmployeeById: async (id: number): Promise<Employee> => {
-    const response = await axiosInstance.get(`http://127.0.0.1:8000/api/users/${id}`);
+    const response = await axiosInstance.get(`/users/${id}`);
     return response.data;
   },
 
   createEmployee: async (employee: CreateEmployeeDto): Promise<Employee> => {
-    const response = await axiosInstance.post('http://127.0.0.1:8000/api/users/', employee);
+    const response = await axiosInstance.post('/users/', employee);
     return response.data;
   },
 
   updateEmployee: async (id: number, employee: Partial<Employee>): Promise<Employee> => {
-    const response = await axiosInstance.put(`http://127.0.0.1:8000/api/users/${id}`, employee);
+    const response = await axiosInstance.put(`/users/${id}`, employee);
     return response.data;
   },
 
   deleteEmployee: async (id: number): Promise<void> => {
-  await axiosInstance.delete(`http://127.0.0.1:8000/api/users/${id}/`);
+    await axiosInstance.delete(`/users/${id}/`);
   },
 };
